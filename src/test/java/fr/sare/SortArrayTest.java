cb5c44a638d9825d1049a6ca67389934e75d9e40
sare <aboudou.samadou.sare@gmail.com>
2024-06-11 13:12:28 +0200

    feat: watermelon test with assertions and mutation test

--- src/test/java/fr/sare/SortArrayTest.java
+++ src/test/java/fr/sare/SortArrayTest.java
@@ -11,7 +11,7 @@ public class SortArrayTest {
     void shouldReturnTrueWhenArrayIsSortedInAscendantOrder() {
         int[] array = {1, 2, 3, 4, 5};
 
-       boolean isSortedInAscendantOrder = isSortedInAscendantOrder(array);
+        boolean isSortedInAscendantOrder = isSortedInAscendantOrder(array);
 
         Assertions.assertTrue(isSortedInAscendantOrder);
     }
@@ -21,9 +21,18 @@ public class SortArrayTest {
     void shouldReturnTrueWhenArrayIsNotSortedInAscendantOrder() {
         int[] array = {5, 4, 3, 2, 1};
 
-        boolean isSortedInAscendantOrder =  isSortedInAscendantOrder(array);
+        boolean isSortedInAscendantOrder = isSortedInAscendantOrder(array);
 
         Assertions.assertFalse(isSortedInAscendantOrder);
+    }
+
+    @Test
+    void shouldReturnTrueWhenArrayWithDuplicateIsSortedInAscendantOrder() {
+        int[] array = {1, 2, 2, 3, 4};
+
+        boolean isSortedInAscendantOrder = isSortedInAscendantOrder(array);
+
+        Assertions.assertTrue(isSortedInAscendantOrder);
     }
 
 }
