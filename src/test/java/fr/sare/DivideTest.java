875bf71552b7f895b6eeba5ec15578ff2ca0623b
sare <aboudou.samadou.sare@gmail.com>
2024-06-11 13:17:21 +0200

    feat: show that test is not sufficent to find bug

---
+++ src/test/java/fr/sare/DivideTest.java
@@ -0,0 +1,16 @@
+package fr.sare;
+
+import org.junit.jupiter.api.Test;
+
+import static fr.sare.Divider.divide;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DivideTest {
+
+    @Test
+    void testDivision() {
+        double result = divide(10, 2);
+
+        assertEquals(5, result);
+    }// There is a bug even if it is tested
+}
