95dc899b1bff62ef0847167c57bcb426c88f48ca
sare <aboudou.samadou.sare@gmail.com>
2024-06-11 12:59:37 +0200

    feat: watermelon test with assertions

--- src/test/java/fr/sare/SortArrayTest.java
+++ src/test/java/fr/sare/SortArrayTest.java
@@ -1,5 +1,6 @@
 package fr.sare;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static fr.sare.SortArray.isSortedInAscendantOrder;
@@ -10,16 +11,19 @@ public class SortArrayTest {
     void shouldReturnTrueWhenArrayIsSortedInAscendantOrder() {
         int[] array = {1, 2, 3, 4, 5};
 
-        isSortedInAscendantOrder(array);
-    }
+       boolean isSortedInAscendantOrder = isSortedInAscendantOrder(array);
 
+        Assertions.assertTrue(isSortedInAscendantOrder);
+    }
 
 
     @Test
     void shouldReturnTrueWhenArrayIsNotSortedInAscendantOrder() {
         int[] array = {5, 4, 3, 2, 1};
 
-        isSortedInAscendantOrder(array);
+        boolean isSortedInAscendantOrder =  isSortedInAscendantOrder(array);
+
+        Assertions.assertFalse(isSortedInAscendantOrder);
     }
 
 }
